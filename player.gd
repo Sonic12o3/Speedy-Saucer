cedab8b00ac0f769b63fb1740b72a9b6dd5e6d1c
Sonic12o3 <neostrom12@gmail.com>
2024-02-08 00:12:19 -0500

    Added input scripting

--- player.gd
+++ player.gd
@@ -1,11 +1,20 @@
 extends RigidBody2D
 
-
+var movespeed = 500;
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	#apply_impulse(Vector2(25, 0))
+	pass
+	
 func _process(delta):
 	pass
+	
+func _physics_process(delta):
+	if Input.is_action_pressed("move_right"):
+		apply_force(Vector2(movespeed, 0))
+	if Input.is_action_pressed("move_left"):
+		apply_force(Vector2(-movespeed, 0))
+	if Input.is_action_pressed("move_up"):
+		apply_force(Vector2(0, -movespeed))
+	if Input.is_action_pressed("move_down"):
+		apply_force(Vector2(0, movespeed))
